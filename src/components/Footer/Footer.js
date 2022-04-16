fd595d04e1093ab0f6f9dcf915a450dad6721922
eshakkhan29 <eshakkhan29@gmail.com>
2022-04-17 03:33:54 +0600

    login and signup mobile responsive

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css'
 const Footer = () => {
     return (
         <div className='mt-5 footer d-flex align-items-center justify-content-center'>
-            <p className='text-center fs-4 footer-text'>&copy; Dental cere || All Rights Reserved</p>
+            <p className='text-center fs-4 footer-text m-0'>&copy; Dental cere || All Rights Reserved</p>
         </div>
     );
 };
